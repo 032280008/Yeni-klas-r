30a222b26b69033bbcef8785516a8458192205c0
032280008 <032280008@ogr.uludag.edu.tr>
2022-11-11 14:22:16 +0300

    first commit

---
+++ 2.py
@@ -0,0 +1,52 @@
+# control flow
+# akışı kontrol et !
+
+#a = 3
+#b = 5
+a , b = 3 , 5
+# code block
+# mesela c dillerinde kod blokları { }
+# if a > { print....}
+a , b = b , a
+a = b 
+if a > b :
+    print("a b'den büyük")
+# blok nesnesi başladı ise kesinlikle altında girinti bekler.
+if 4 < 1 :
+    pass
+if 4 > 1 :
+    print("bu doğru bir durum!")
+
+değişken_adı = 5
+# int - integer - tam sayı tam sayı tipli bir değişken 
+print( (type) değişken adı ) 
+kesir = 3 / 7 
+# float kesir tipli değişken
+# ondalık olarak nokta(.) işaret kullanılır!
+
+print(type(kesir) , kesir)
+
+
+
+x = "5" # str
+y = 5 # int
+z =5.0 # float 
+# print( x==y )
+# x == y ? eşit değildir. print (type(x) == typee(y) )
+# y == z ? eşit 
+
+t = False 
+t = True
+# boll - boolean / doğru-yanlış tipli değişken.
+# Doğru True 
+# Yanlış False 
+print(type( t ) ) 
+
+# doğru yanlış tipi değeri ilk harfi büyük yazılır.
+# True = 5 anahtar kelime olduğu için değişken olmaz.
+# true = 5 değişken olabilir.
+
+a = 100 
+b = " bursaspor! "
+print ( a * b )
+
